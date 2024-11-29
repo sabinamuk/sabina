a01d0a737e5969fda66ee0a91e4c8016f3e4b75a
sabinamuk <stm1573@icloud.com>
2024-11-29 15:10:26 +0600

    Create задание 2 .cs

---
+++ алгоритмизация и программирование/1 семестр/лаб 3/задание 2 .cs
@@ -0,0 +1,49 @@
+/******************************************************************************
+30.09.24-Определить в последовательности минимальный размер 
+подпоследовательности состоящей из 0
+*******************************************************************************/
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int c = 0, min = -1, n, num;
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 0)
+            {
+                c++;
+            }
+            else
+            {
+                if (c > 0)
+                {
+                    if (min == -1 || c < min)
+                    {
+                        min = c;
+                    }
+                }
+                c = 0;
+            }
+        }
+        if (c > 0)
+        {
+            if (min == -1 || c < min)
+            {
+                min = c;
+            }
+        }
+        if (min == -1)
+        {
+            Console.WriteLine(0);
+        }
+        else
+        {
+            Console.WriteLine(min);
+        }
+    }
+}
