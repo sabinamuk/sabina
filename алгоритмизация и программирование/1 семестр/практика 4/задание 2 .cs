07a45066269ef7a28ff58f4438351ee8f6597ce3
sabinamuk <stm1573@icloud.com>
2024-12-29 00:34:43 +0600

    Update задание 2 .cs

--- алгоритмизация и программирование/1 семестр/практика 4/задание 2 .cs
+++ алгоритмизация и программирование/1 семестр/практика 4/задание 2 .cs
@@ -2,50 +2,40 @@
 практика - 4 (16.10.24) - упаковки молока
 *******************************************************************************/
 using System;
-class HelloWorld
+using System.Globalization;
+namespace ConsoleApp1
 {
-    static void Main()
+    internal class Program
     {
-        int N = Convert.ToInt32(Console.ReadLine());
-        double[] prices = new double[N];
-
-        for (int i = 0; i < N; i++)
+        static void Main(string[] args)
         {
-            int[] cont1 = new int[3];
-            int[] cont2 = new int[3];
-            for (int j = 0; j < 3; j++)
+            int Fline = Convert.ToInt32(Console.ReadLine());
+            double res = 10000;
+            int cnt = 0;
+            for (int i = 0; i < Fline; i++)
             {
-                Console.Write("Введите размеры упаковки 1: ");
-                cont1[j] = Convert.ToInt32(Console.ReadLine());
+                string line = Console.ReadLine();
+                string[] varArr = line.Split(' ');
+                int x1 = int.Parse(varArr[0]);
+                int y1 = int.Parse(varArr[1]);
+                int z1 = int.Parse(varArr[2]);
+                int x2 = int.Parse(varArr[3]);
+                int y2 = int.Parse(varArr[4]);
+                int z2 = int.Parse(varArr[5]);
+                double c1 = double.Parse(varArr[6], CultureInfo.InvariantCulture);
+                double c2 = double.Parse(varArr[7], CultureInfo.InvariantCulture);
+                double s1 = 2 * (x1 * y1 + y1 * z1 + x1 * z1);
+                double s2 = 2 * (x2 * y2 + y2 * z2 + x2 * z2);
+                double v1 = x1 * y1 * z1;
+                double v2 = x2 * y2 * z2;
+                double price = (-c1 + (s1 * c2) / s2) / (-((v1 - s1) / 1000) + (s1 * ((v2 - s2) / 1000)) / s2);
+                if (price < res)
+                {
+                    res = Math.Min(res, price);
+                    cnt = i + 1;
+                }
             }
-            for (int j = 0; j < 3; j++)
-            {
-                Console.Write("Введите размеры упаковки 2: ");
-                cont2[j] = Convert.ToInt32(Console.ReadLine());
-            }
-            Console.Write("Введите стоимость упаковки 1: ");
-            double c1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите стоимость упаковки 2: ");
-            double c2 = Convert.ToDouble(Console.ReadLine());
-
-            double v1 = (double)(cont1[0] * cont1[1] * cont1[2]) / 1000;
-            double v2 = (double)(cont2[0] * cont2[1] * cont2[2]) / 1000;
-            int s1 = 2 * (cont1[0] * cont1[1] + cont1[0] * cont1[2] + cont1[1] * cont1[2]);
-            int s2 = 2 * (cont2[0] * cont2[1] + cont2[0] * cont2[2] + cont2[1] * cont2[2]);
-
-            double literPrice = Math.Round((c1 - s1 * ((c2 * v1 - c1 * v2) / (s2 * v1 - s1 * v2))) / v1, 2);
-            prices[i] = literPrice;
+            Console.WriteLine($"{cnt} {Math.Round(res, 2)}");
         }
-        double minPrice = prices[0];
-        int t = 1;
-        for (int n = 1; n < N; n++)
-        {
-            if (prices[n] < minPrice)
-            {
-                minPrice = prices[n];
-                t = n + 1;
-            }
-        }
-        Console.WriteLine("{0} {1:f2}", t, minPrice);
     }
 }
