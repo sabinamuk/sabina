9130ec92c35e503d1d1cc91df8887544b8d7ff96
sabinamuk <stm1573@icloud.com>
2025-02-21 17:43:59 +0600

    Update лаб 1 .cs

--- алгоритмизация и программирование/2 семестр/лаб 1 .cs
+++ алгоритмизация и программирование/2 семестр/лаб 1 .cs
@@ -2,65 +2,61 @@
 лаб 1 - (07.02.25) телефонный справочник 
 ****************************************/
 using System;
+using System.Linq;
 class PhoneNumberBook
 {
-    public string Number { get; set; }
-    public string NameOperator { get; set; }
-    public string Year { get; set; }
-    public PhoneNumberBook(string number, string nameoperator, string year)
+    public string name;
+    public string city;
+    public PhoneNumber[] numbers;
+    public PhoneNumberBook(string name, string city, PhoneNumber[] numbers)
     {
-        Number = number;
-        NameOperator = nameoperator;
-        Year = year;
+        this.name = name;
+        this.city = city;
+        this.numbers = numbers;
     }
 }
-class Contact
+class PhoneNumber
 {
-    public string Name { get; set; }
-    public string City { get; set; }
-    public PhoneNumberBook[] Numbers { get; set; }
-    public Contact(string name, string city, PhoneNumberBook[] numbers)
+    public string number;
+    public string operat;
+    public string year;
+    public PhoneNumber(string number, string operat, string year)
     {
-        Name = name;
-        City = city;
-        Numbers = numbers;
+        this.number = number;
+        this.operat = operat;
+        this.year = year;
     }
 }
 class Program
 {
     static void Main()
     {
-        Console.Write("Введите количество контактов: ");
+        Console.Write("Введите число контактов: ");
         int n = Convert.ToInt32(Console.ReadLine());
-        Contact[] contacts = new Contact[n];
-
+        PhoneNumberBook[] contacts = new PhoneNumberBook[n];
         for (int i = 0; i < n; i++)
         {
-            Console.WriteLine($"Введите данные контакта {i + 1} (ФИО город номер1 оператор1 год1 номер2 оператор2 год2...):");
-            string[] data = Console.ReadLine().Split(' ');
-
-            if (data.Length < 4)
+            Console.WriteLine($"Введите данные о контакте {i + 1} (ФИО(Ф_И_О) город номер1 оператор1 год1 номер2 оператор2 год2 ...):");
+            string[] inform = Console.ReadLine().Split(' ');
+            if (inform.Length < 4)
             {
-                Console.WriteLine("Недостаточно данных.");
+                Console.WriteLine("Недостаточно данных для контакта.");
                 i--;
                 continue;
             }
-
-            string name = data[0];
-            string city = data[1];
-
-            PhoneNumberBook[] numbers = new PhoneNumberBook[(data.Length - 2) / 3];
-            for (int j = 0, k = 2; j < numbers.Length; j++, k += 3)
+            string name = inform[0];
+            string city = inform[1];
+            PhoneNumber[] numbers = new PhoneNumber[(inform.Length - 2) / 3];
+            for (int j = 0, z = 2; j < numbers.Length; j++, z += 3)
             {
-                numbers[j] = new PhoneNumberBook(data[k], data[k + 1], data[k + 2]);
+                numbers[j] = new PhoneNumber(inform[z], inform[z + 1], inform[z + 2]);
             }
-
-            contacts[i] = new Contact(name, city, numbers);
+            contacts[i] = new PhoneNumberBook(name, city, numbers);
         }
         int action;
         do
         {
-            Console.WriteLine("Выберите действие:\n1 – Заполнение | 2 – Вывод данных | 3 – Выборка по оператору | 4 – Выборка по имени | 5 – Выборка по номеру | 6 – Выход");
+            Console.WriteLine("Выберите действие:\n1 – Заполнение | 2 – Вывод данных | 3 – Выборка по оператору | 4 – Выборка по ФИО | 5 – Выборка по номеру | 6 – Выход ");
             action = Convert.ToInt32(Console.ReadLine());
             switch (action)
             {
@@ -68,57 +64,57 @@ class Program
                     Console.WriteLine("Данные получены");
                     break;
                 case 2:
-                    Console.WriteLine("---Контакты---");
-                    for (int i = 0; i < n; i++)
+                    Console.WriteLine("Контакты:");
+                    foreach (var contact in contacts)
                     {
-                        Console.Write($"{contacts[i].Name} {contacts[i].City} ");
-                        for (int j = 0; j < contacts[i].Numbers.Length; j++)
+                        Console.Write($"{contact.name} {contact.city} ");
+                        foreach (var num in contact.numbers)
                         {
-                            Console.Write($"{contacts[i].Numbers[j].Number} ({contacts[i].Numbers[j].NameOperator}, {contacts[i].Numbers[j].Year}) ");
+                            Console.Write($"{num.number} ({num.operat}, {num.year})  ");
                         }
                         Console.WriteLine();
                     }
                     break;
                 case 3:
                     Console.Write("Введите оператора: ");
-                    string nameoperator = Console.ReadLine();
-                    for (int i = 0; i < n; i++)
+                    string operat = Console.ReadLine();
+                    foreach (var contact in contacts)
                     {
-                        for (int j = 0; j < contacts[i].Numbers.Length; j++)
+                        foreach (var num in contact.numbers)
                         {
-                            if (contacts[i].Numbers[j].NameOperator.Equals(nameoperator, StringComparison.OrdinalIgnoreCase))
+                            if (num.operat.Equals(operat, StringComparison.OrdinalIgnoreCase))
                             {
-                                Console.Write($"{contacts[i].Name} {contacts[i].City} {contacts[i].Numbers[j].Number} ({contacts[i].Numbers[j].NameOperator}, {contacts[i].Numbers[j].Year})\n");
+                                Console.WriteLine($"{contact.name} {contact.city} {num.number} ({num.operat}, {num.year})");
                             }
                         }
                     }
                     break;
                 case 4:
-                    Console.Write("Введите ФИО контакта: ");
+                    Console.Write("Введите ФИО: ");
                     string name = Console.ReadLine();
-                    for (int i = 0; i < n; i++)
+                    foreach (var contact in contacts)
                     {
-                        if (contacts[i].Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                        if (contact.name.Equals(name, StringComparison.OrdinalIgnoreCase))
                         {
-                            Console.Write($"{contacts[i].Name} {contacts[i].City} ");
-                            for (int j = 0; j < contacts[i].Numbers.Length; j++)
+                            Console.Write($"{contact.name} {contact.city} ");
+                            foreach (var num in contact.numbers)
                             {
-                                Console.Write($"{contacts[i].Numbers[j].Number} ({contacts[i].Numbers[j].NameOperator}, {contacts[i].Numbers[j].Year}) ");
+                                Console.Write($"{num.number} ({num.operat}, {num.year})  ");
                             }
                             Console.WriteLine();
                         }
                     }
                     break;
                 case 5:
-                    Console.Write("Введите номер телефона: ");
+                    Console.Write("Введите номер: ");
                     string phoneNumber = Console.ReadLine();
-                    for (int i = 0; i < n; i++)
+                    foreach (var contact in contacts)
                     {
-                        for (int j = 0; j < contacts[i].Numbers.Length; j++)
+                        foreach (var num in contact.numbers)
                         {
-                            if (contacts[i].Numbers[j].Number.Equals(phoneNumber))
+                            if (num.number == phoneNumber)
                             {
-                                Console.Write($"{contacts[i].Name} {contacts[i].City} {contacts[i].Numbers[j].Number} ({contacts[i].Numbers[j].NameOperator}, {contacts[i].Numbers[j].Year})\n");
+                                Console.WriteLine($"{contact.name} {contact.city} {num.number} ({num.operat}, {num.year})");
                             }
                         }
                     }
